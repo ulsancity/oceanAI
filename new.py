d962bd6000aaad2767f2e933b344cb9567770ca8
Github ulsancity <air2228@naver.com>
2025-03-11 16:54:39 +0900

    new

---
+++ new.py
@@ -0,0 +1,27 @@
+
+lst = [10, 20, 30]
+total= sum(lst)
+print(total)
+
+print('hello world')
+name = '이병현'
+age = 23
+print('안녕! 나는', name, '이야. 나는 나이가', age, '살이야.')
+name = '이병순'
+age = 22
+print('안녕! 나는', name, '이야. 나는 나이가', age, '살이야.')
+
+print(2**2)
+print(2%1.5)
+print(3**0.5)
+print(3**(1/3))
+
+num = 85
+print(type(num))
+
+
+
+
+
+
+
