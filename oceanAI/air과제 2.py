44499b18c8750b19d7b8efefb566fb44249a01ed
Github ulsancity <air2228@naver.com>
2025-03-16 21:24:50 +0900

    ai과제

---
+++ oceanAI/air과제 2.py
@@ -0,0 +1,30 @@
+print('나의 이름은 :', '이병현')
+print('나의 나이는 :', 23)
+print('나의 키는', 172, 'cm입니다.')
+print('10+20 =', 10+20)
+print('10*20 =', 10*20)
+name = '이병현'
+print('나의 이름은 :', name)
+age = 23
+print('나의 나이는 :', age)
+height = 172
+print('나의 키는', height, 'cm 입니다.')
+sum = 10+20
+print('10+20 =', sum)
+mult = 10*20
+print('10*20=', mult)
+width = 20
+height = 40
+width = 30
+area = width * height
+print('사각형의 면적', area)
+print(123*456)
+print(1357+2468)
+print(5**4)
+print(10/4)
+print(10//5)
+print(10%5)
+print(5%2)
+print(2**(1/2))
+print(3**(1/2))
+
