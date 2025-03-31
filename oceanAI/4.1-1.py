73f0912c3e7cfbd072929a08f3cde83be7aa876b
ulsancity <air2228@naver.com>
2025-03-31 20:02:06 +0900

    Add files via upload

---
+++ oceanAI/4.1-1.py
@@ -0,0 +1,60 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/jeff4/OneDrive/바탕 화면/4.1.py
+40
+60
+30
+60
+70
+50
+70
+90
+20
+7
+3 * 2 = 6
+3 * 3 = 9
+3 * 4 = 12
+3 * 5 = 15
+3 * 6 = 18
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+5 * 6 = 30
+7 * 2 = 14
+7 * 3 = 21
+7 * 4 = 28
+7 * 5 = 35
+7 * 6 = 42
+I like pancakes.
+I like kiwi juice.
+I like espresso.
+I love pancakes.
+I love kiwi juice.
+I love espresso.
+리스트 원소들: [10, 20, 30, 50, 60]
+리스트 원소들의 합: 170
+리스트 원소들: [10, 20, 30, 50, 60]
+리스트 원소들 중 최댓값: 60
+5개의 수를 입력하세요: 12 3 4 56 78
+합: 153
+평균: 30.6
+최댓값: 78
+최솟값: 3
+n을 입력하세요: 2
+2개의 수를 입력하세요: 12 34
+합: 46.0
+평균: 23.00
+표준편차: 11.00
+Have
+beautiful
+day
+animals = ['dog', 'cat', 'tiger', 'lion']
+animals = ['cat', 'tiger', 'lion', 'dog']
+I love cat.
+I love tiger.
+I love lion.
+I love dog.
+new_s_list = ['abc', 'bcd', 'abba', 'cddc', 'opq']
+output = aabbbccccccaacccfg
